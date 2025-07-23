63c9db66cd08e6195dadd94ae89b3f55a6c70862
HaraldWik <wikhallharald@gmail.com>
2025-07-23 15:26:23 +0200

    Updated build.zig and .zon and added README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+> **Note**
+> Does not compile since breaking changes with the SDL3 binding
--- build.zig
+++ build.zig
@@ -3,45 +3,49 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+
+    const sdl_mod = b.dependency("sdl3", .{ .target = target, .optimize = optimize, .ext_image = true }).module("sdl3");
+
+    const gl_mod = @import("zigglgen").generateBindingsModule(b);
+    // .version = .@"4.6",
+    // .profile = .core,
+    // .extensions = &.{ .ARB_clip_control, .NV_scissor_exclusive },
+
+    const numz_mod = b.dependency("numz", .{ .target = target, .optimize = optimize }).module("numz");
+
+    const exe = b.addExecutable(.{
+        .name = "GameEngineZigTest",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "sdl3", .module = sdl_mod },
+                .{ .name = "gl", .module = gl_mod },
+                .{ .name = "numz", .module = numz_mod },
+            },
+        }),
     });
-
-    const exe = b.addExecutable(.{ .name = "vk", .root_module = exe_mod });
-
-    const sdl_dep = b.dependency("sdl3", .{ .target = target, .optimize = optimize, .ext_image = true });
-    exe.root_module.addImport("sdl3", sdl_dep.module("sdl3"));
-
-    const gl_bindings = @import("zigglgen").generateBindingsModule(b, .{
-        .api = .gl,
-        .version = .@"4.6",
-        .profile = .core,
-        .extensions = &.{ .ARB_clip_control, .NV_scissor_exclusive },
-    });
-    exe.root_module.addImport("gl", gl_bindings);
-
-    const numz_dep = b.dependency("numz", .{ .target = target, .optimize = optimize });
-    exe.root_module.addImport("numz", numz_dep.module("numz"));
-
-    exe.linkSystemLibrary("vulkan");
 
     b.installArtifact(exe);
 
+    const run_step = b.step("run", "Run the app");
+
     const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
     run_cmd.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
     });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_exe_tests.step);
 }
